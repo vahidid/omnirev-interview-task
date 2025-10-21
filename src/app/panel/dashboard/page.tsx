07930e0fb166017d5832c2361ff5de1cf6653177
Vahid Hasani <vahidhasani09@gmail.com>
2025-10-21 18:10:15 +0330

    feat: add group button for filter

--- src/app/panel/dashboard/page.tsx
+++ src/app/panel/dashboard/page.tsx
@@ -1,11 +1,35 @@
 import CompaniesChart from "@/app/features/dashboard/companies-chart";
 import HighValueCustomer from "@/app/features/dashboard/high-value-customers";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Bold, Italic, Underline } from "lucide-react";
 
 export default function Page() {
 	return (
 		<div className="flex flex-1 flex-col gap-4 p-4">
-			<div className="flex-1">
-				<h1>Dashboard</h1>
+			<div className="flex-1 flex">
+				<h1 className="flex-1 text-3xl font-bold">Dashboard</h1>
+				<div className="flex-2">
+					<ToggleGroup type="single" variant="outline" spacing={2}>
+						<ToggleGroupItem
+							value="bold"
+							className="data-[state=on]:border-primary"
+						>
+							Yesterday
+						</ToggleGroupItem>
+						<ToggleGroupItem
+							value="italic"
+							className="data-[state=on]:border-primary"
+						>
+							7 Days
+						</ToggleGroupItem>
+						<ToggleGroupItem
+							value="strikethrough"
+							className="data-[state=on]:border-primary"
+						>
+							30 Days
+						</ToggleGroupItem>
+					</ToggleGroup>
+				</div>
 			</div>
 			<div className="grid auto-rows-min gap-4 md:grid-cols-2">
 				<CompaniesChart />
